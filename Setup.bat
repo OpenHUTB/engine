87d5ff6fb0b8744ae59048460e3a0ca09acc1a95
nongfugengxia <nongfugengxia@163.com>
2023-08-29 08:01:19 +0800

    注释环境配置

--- Setup.bat
+++ Setup.bat
@@ -9,7 +9,7 @@ for %%P in (%*) do if /I "%%P" == "--force" goto no_prompt_argument
 set PROMPT_ARGUMENT=--prompt
 :no_prompt_argument
 
-rem Sync the dependencies...
+rem 同步依赖...
 .\Engine\Binaries\DotNET\GitDependencies.exe %PROMPT_ARGUMENT% %*
 if ERRORLEVEL 1 goto error
 
@@ -22,19 +22,19 @@ echo #!/bin/sh >.git\hooks\post-merge
 echo Engine/Binaries/DotNET/GitDependencies.exe %* >>.git\hooks\post-merge
 :no_git_hooks_directory
 
-rem Install prerequisites...
+rem 安装先决条件...
 echo Installing prerequisites...
 start /wait Engine\Extras\Redist\en-us\UE4PrereqSetup_x64.exe /quiet
 
-rem Register the engine installation...
+rem 注册引擎安装...
 if not exist .\Engine\Binaries\Win64\UnrealVersionSelector-Win64-Shipping.exe goto :no_unreal_version_selector
 .\Engine\Binaries\Win64\UnrealVersionSelector-Win64-Shipping.exe /register
 :no_unreal_version_selector
 
-rem Done!
+rem 完成!
 goto :end
 
-rem Error happened. Wait for a keypress before quitting.
+rem 发生错误，等待按键来退出。
 :error
 pause
 

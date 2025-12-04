816948c6f7a7b1d9a34bad8664e00c47c9a828a0
donghaiwang <whd@hutb.edu.cn>
2025-12-04 09:58:51 +0800

    使用缓存的ue4-gitdeps

--- Build.bat
+++ Build.bat
@@ -6,7 +6,27 @@ chcp 65001
 :: call setEnv64.bat
 :: %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+if exist "%programfiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
+	call "%programfiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+) else (
+	call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+
+
+:: 检查是否存在 ..\..\..\dependencies\ue4-gitdeps 目录，存在则拷贝到 .git\ue4-gitdeps
+for %%I in ("%~dp0..\..\..") do set "out_dir=%%~fI"
+set dep_dir="%out_dir%dependencies\ue4-gitdeps"
+echo dep_dir=%dep_dir%
+if exist "%dep_dir%" (
+	echo existing dependencies\ue4-gitdeps found.
+	if not exist "%~dp0\.git\ue4-gitdeps" (
+		echo Copying ue4-gitdeps from dependencies to .git directory...
+		xcopy /E /I /Y "%~dp0..\..\..\dependencies\ue4-gitdeps" "%~dp0\.git\ue4-gitdeps"
+		echo Copy dependencies complete.
+	)
+)
+:: pause
+:: exit /b 0
 
 :: 使用 --force 选项来跳过: Checking dependencies... overwrite your changes (y/n)
 call Setup.bat --force
